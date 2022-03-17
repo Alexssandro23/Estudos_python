c20c0b211d741cade51e85ac8ea13bd622bac9da
alexssandro <allexbottelho2003@gmail.com>
2022-03-17 19:49:07 -0300

    exercicio adicionado e realizado

--- ex_1.py
+++ ex_1.py
@@ -1,1 +1,12 @@
-# exercicio de teste
+V_casa=float(input("insira o valor da casa...:"))
+salario=float(input("informe seu salário...:"))
+ano=int(input("por quantos anos voçê deseja pagar...:"))
+
+parcela= V_casa/(ano*12)
+print(parcela)
+print(salario*0.3)
+if salario*0.3 > parcela:
+    print("emprestimo autorizado !!!")
+else:
+    print("emprestimo negado !!!")
+    
