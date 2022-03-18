fce31415de0a1bf0a18aea4186e70f127d191763
alexssandro <allexbottelho2003@gmail.com>
2022-03-18 01:30:08 -0300

    exercicio concluido

---
+++ ex_3.py
@@ -0,0 +1,8 @@
+n1=int(input("digite um numero..:"))
+n2=int(input("digite outro numero..:"))
+if n1 > n2:
+    print("o primeiro numero é maior !!!")
+elif n1 < n2:
+    print("o segundo numero é maior !!!")
+else:
+    print("os dois numeros são iguais !!!")        
